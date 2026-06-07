b861622966e211ba5c2d7f91973f41f4e7aead62
megaclassdigital-cloud <megaclassdigital@gmail.com>
2026-06-07 20:02:49 +0700

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next';
-import './globals.css';
+import '/globals.css';
 
 export const metadata: Metadata = {
   title: 'Solusi Box Pendingin Topre untuk Distribusi Frozen & Chilled',
   description: 'Konsultasikan kebutuhan refrigerated vehicle, box pendingin, dan solusi distribusi produk frozen maupun chilled bersama sales Topre.',
-  metadataBase: new URL('https://topre-refrigerated-vehicle.vercel.app'),
+  metadataBase: new URL('https://topre-b6cypvhg0-megaclassdigital-2415s-projects.vercel.app/'),
   icons: {
     icon: '/favicon.ico', // Tambahkan ini (letakkan file di dalam folder /public)
     shortcut: '/favicon.ico',
